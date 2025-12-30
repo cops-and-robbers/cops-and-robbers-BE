7e998867836c40f0bb9e962a248187bf926bb48f
chxghee <francis001021@gmail.com>
2025-12-30 20:20:17 +0900

    init: spring project setup

---
+++ src/main/java/com/team/cops_and_robbers/CopsAndRobbersApplication.java
@@ -0,0 +1,13 @@
+package com.team.cops_and_robbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopsAndRobbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopsAndRobbersApplication.class, args);
+	}
+
+}
