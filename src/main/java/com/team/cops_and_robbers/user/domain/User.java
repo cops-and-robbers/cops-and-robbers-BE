d49437d6cb99d527fb91e817f4f4a587a50f53b5
chxghee <francis001021@gmail.com>
2026-01-10 12:43:52 +0900

    feat: user엔티티 소셜 로그인 관련 필드 추가 및 ymal파일 설정 추가

--- src/main/java/com/team/cops_and_robbers/user/domain/User.java
+++ src/main/java/com/team/cops_and_robbers/user/domain/User.java
@@ -1,12 +1,7 @@
 package com.team.cops_and_robbers.user.domain;
 
 import com.team.cops_and_robbers.common.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +9,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_users_social_id_type",
+                        columnNames = {"socialId", "socialType"}
+                )
+        }
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -25,13 +28,20 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String deviceId;
+    @Column(nullable = false, length = 100)
+    private String socialId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private SocialType socialType;
 
     @Column(nullable = false, unique = true, length = 20)
     private String nickname;
 
-    private String pushToken;
+    @Column(nullable = false)
+    private String deviceId;
+
+    private String fcmToken;
 
     @Column(nullable = false)
     private boolean allowGamePush;
