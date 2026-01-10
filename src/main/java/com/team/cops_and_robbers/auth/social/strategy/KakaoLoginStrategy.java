3c00acc264c84e379006b74240c193454d0f52dc
chxghee <francis001021@gmail.com>
2026-01-10 19:55:38 +0900

    feat: 소셜 로그인 전략 분리 및 구현체 임시 작성 [#6]

---
+++ src/main/java/com/team/cops_and_robbers/auth/social/strategy/KakaoLoginStrategy.java
@@ -0,0 +1,25 @@
+package com.team.cops_and_robbers.auth.social.strategy;
+
+import com.team.cops_and_robbers.user.domain.SocialType;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class KakaoLoginStrategy implements SocialLoginStrategy {
+
+    @Override
+    public String validateAndGetSocialId(String socialIdToken) {
+
+        //== kakao login logic 필요 ==//
+        // -> 클라이언트에서 받은 idToken으로 소셜 로그인 리소스 서버로 부터 id 를 가져온다.
+
+        ThreadLocalRandom random = ThreadLocalRandom.current(); // 임시 social id 발급용
+        return "KAKAO_SOCIAL_ID" + random.nextInt(100_000);
+    }
+
+    @Override
+    public SocialType getSocialType() {
+        return SocialType.KAKAO;
+    }
+}
