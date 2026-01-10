c3cd5e9d9ab3b48cbcc63b865e289fac9f885af9
chxghee <francis001021@gmail.com>
2026-01-11 00:57:07 +0900

    feat: token 생성 및 저장 로직 구현 [#6]

--- src/main/java/com/team/cops_and_robbers/auth/application/AuthService.java
+++ src/main/java/com/team/cops_and_robbers/auth/application/AuthService.java
@@ -3,7 +3,10 @@ package com.team.cops_and_robbers.auth.application;
 
 import com.team.cops_and_robbers.auth.application.dto.command.LoginCommand;
 import com.team.cops_and_robbers.auth.application.dto.result.LoginResult;
+import com.team.cops_and_robbers.auth.domain.Tokens;
 import com.team.cops_and_robbers.auth.exception.AuthException;
+import com.team.cops_and_robbers.auth.jwt.JwtTokenProvider;
+import com.team.cops_and_robbers.auth.repository.RefreshTokenRepository;
 import com.team.cops_and_robbers.auth.social.strategy.SocialLoginStrategy;
 import com.team.cops_and_robbers.common.exception.ApplicationException;
 import com.team.cops_and_robbers.user.domain.SocialType;
@@ -23,19 +26,29 @@ public class AuthService {
 
     private static final int MAXIMUM_NICKNAME_GENERATE_RETRY_COUNT = 10;
 
+
     private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtTokenProvider jwtTokenProvider;
     private final RandomNicknameGenerator randomNicknameGenerator;
     private final Map<SocialType, SocialLoginStrategy> socialLoginStrategies;
 
+    /**
+     * 1. 소셜 로그인 진행
+     *  - 처음 로그인한 사용자의 경우 랜덤 닉네임 부여 후 DB 추가
+     *  - 기 가입자는 로그인한 핸드폰의 device, fcm 정보로 DB 업데이트
+     */
     @Transactional
     public LoginResult login(LoginCommand command) {
         String socialId = getSocialId(command.socialType(), command.idToken());
         AuthUserData authUserData = findOrRegisterUser(command, socialId);
+        User user = authUserData.user;
+        Tokens tokens = issueTokens(user);
+
         return new LoginResult(
-                authUserData.user,
-                "ACCESS_TOKEN",
-                "REFRESH_TOKEN",
-                authUserData.isNewUser
+                user,
+                authUserData.isNewUser,
+                tokens
         );
     }
 
@@ -85,6 +98,19 @@ public class AuthService {
         return nickname;
     }
 
-    private record AuthUserData(User user, boolean isNewUser) {}
+    private Tokens issueTokens(User loginUser) {
+        String accessToken = jwtTokenProvider.createAccessToken(loginUser);
+        String refreshToken = jwtTokenProvider.createRefreshToken(loginUser);
+        refreshTokenRepository.save(loginUser.getId(), refreshToken, jwtTokenProvider.getRefreshTokenExpirationMillis());
+        return new Tokens(accessToken, refreshToken);
+    }
 
+
+
+
+
+    private record AuthUserData(
+            User user,
+            boolean isNewUser
+    ) {}
 }
--- src/main/java/com/team/cops_and_robbers/auth/application/dto/result/LoginResult.java
+++ src/main/java/com/team/cops_and_robbers/auth/application/dto/result/LoginResult.java
@@ -1,11 +1,11 @@
 package com.team.cops_and_robbers.auth.application.dto.result;
 
+import com.team.cops_and_robbers.auth.domain.Tokens;
 import com.team.cops_and_robbers.user.domain.User;
 
 public record LoginResult(
         User user,
-        String accessToken,
-        String refreshToken,
-        boolean isNewUser
+        boolean isNewUser,
+        Tokens tokens
 ) {
 }
