5c1a32748dfe1c08907b364bffaa52b37bf483fc
chxghee <francis001021@gmail.com>
2026-01-11 18:59:23 +0900

    feat: 인터셉터 설정 작성 [#6]

---
+++ src/main/java/com/team/cops_and_robbers/common/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.team.cops_and_robbers.common.config;
+
+import com.team.cops_and_robbers.auth.presentation.interceptor.AuthInterceptor;
+import com.team.cops_and_robbers.auth.presentation.resolver.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/api/auth/**"
+                );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
