b405910b5921a5b2e7dd7fd3ab290fe13b262069
hwangsea <134906042+hwangsea@users.noreply.github.com>
2026-01-14 00:55:29 +0900

    feat: 팀 랜덤 배정 기능 추가

--- src/main/java/com/team/cops_and_robbers/game/participant/domain/Team.java
+++ src/main/java/com/team/cops_and_robbers/game/participant/domain/Team.java
@@ -1,6 +1,18 @@
-package com.team.cops_and_robbers.game.participant.domain;
+    package com.team.cops_and_robbers.game.participant.domain;
 
-public enum Team {
-    POLICE,
-    ROBBER
-}
+    import java.util.Arrays;
+    import java.util.Collections;
+    import java.util.List;
+
+    public enum Team {
+        POLICE,
+        ROBBER;
+
+        private static final List<Team> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+        private static final int SIZE = VALUES.size();
+        private static final java.util.Random RANDOM = new java.util.Random();
+
+        public static Team getRandomTeam() {
+            return VALUES.get(RANDOM.nextInt(SIZE));
+        }
+    }
