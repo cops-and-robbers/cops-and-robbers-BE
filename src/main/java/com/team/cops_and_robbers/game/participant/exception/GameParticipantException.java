0addf035f0344d4d9373537e78fdbf53bc9542b0
HyerimH <gofla452@naver.com>
2026-02-09 16:21:56 +0900

    feat: 체포/탈옥을 위한 GameParticipant 예외 추가 [#24]

--- src/main/java/com/team/cops_and_robbers/game/participant/exception/GameParticipantException.java
+++ src/main/java/com/team/cops_and_robbers/game/participant/exception/GameParticipantException.java
@@ -20,7 +20,14 @@ public enum GameParticipantException implements ExceptionCode {
     NOT_HOST(HttpStatus.FORBIDDEN, "호스트 권한 필요", "게임을 시작할 수 있는 권한이 없습니다. 방장만 게임을 시작할 수 있습니다."),
     INVALID_TEAM_COMPOSITION(HttpStatus.BAD_REQUEST, "팀 구성 오류", "게임을 시작하려면 경찰과 도둑 팀에 각각 최소 1명 이상의 참가자가 필요합니다."),
     NOT_ALL_READY(HttpStatus.BAD_REQUEST, "준비 미완료", "모든 참가자가 준비 상태여야 게임을 시작할 수 있습니다."),
-    NOT_ROBBER_TEAM(HttpStatus.BAD_REQUEST, "도둑 팀이 아님", "도둑 팀만 위치를 전송할 수 있습니다.");
+    NOT_ROBBER_TEAM(HttpStatus.BAD_REQUEST, "도둑 팀이 아님", "도둑 팀만 위치를 전송할 수 있습니다."),
+    HOST_CANNOT_UNREADY(HttpStatus.BAD_REQUEST, "방장 레디 해제 불가", "방장은 항상 준비 상태여야 합니다."),
+
+    ONLY_POLICE_CAN_ARREST(HttpStatus.BAD_REQUEST, "경찰만이 체포 가능", "경찰 팀만 도둑을 체포할 수 있습니다."),
+    ONLY_ROBBER_CAN_BE_ARRESTED(HttpStatus.BAD_REQUEST, "도둑만을 체포 가능", "도둑 팀만 체포될 수 있습니다."),
+    ONLY_ROBBER_CAN_ESCAPE(HttpStatus.BAD_REQUEST, "도둑만 탈옥 가능", "도둑 팀만 탈옥할 수 있습니다."),
+    ALREADY_ARRESTED(HttpStatus.BAD_REQUEST, "이미 체포됨", "이미 수감된 도둑입니다."),
+    NOT_JAILED(HttpStatus.BAD_REQUEST, "수감되지 않음", "수감된 상태에서만 탈옥할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String title;
