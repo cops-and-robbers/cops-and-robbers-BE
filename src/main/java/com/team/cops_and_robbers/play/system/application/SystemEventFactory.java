1b77115c4f77704263c6427dd31b80ef2ee68491
HyerimH <gofla452@naver.com>
2026-02-09 16:17:00 +0900

    feat: SystemEventFactory 체포/탈출 추가 [#24]

--- src/main/java/com/team/cops_and_robbers/play/system/application/SystemEventFactory.java
+++ src/main/java/com/team/cops_and_robbers/play/system/application/SystemEventFactory.java
@@ -1,5 +1,6 @@
 package com.team.cops_and_robbers.play.system.application;
 
+import com.team.cops_and_robbers.game.participant.domain.GameParticipant;
 import com.team.cops_and_robbers.play.system.domain.SystemEvent;
 import com.team.cops_and_robbers.play.system.domain.SystemEventData;
 import com.team.cops_and_robbers.play.system.domain.SystemEventType;
@@ -18,5 +19,15 @@ public class SystemEventFactory {
     public SystemEvent createRobberLocationRevealEvent(Long gameId, List<SystemEventData.RobberLocation> locations) {
         SystemEventData.RobberLocationRevealData data = SystemEventData.RobberLocationRevealData.of(locations);
         return SystemEvent.of(gameId, SystemEventType.ROBBER_LOCATION_REVEAL, data);
+    }
+
+    public SystemEvent createArrestEvent(Long gameId, GameParticipant police, GameParticipant robber, int remainingThieves) {
+        SystemEventData.ArrestData data = SystemEventData.ArrestData.of(police, robber, remainingThieves);
+        return SystemEvent.of(gameId, SystemEventType.ARREST, data);
+    }
+
+    public SystemEvent createEscapeEvent(Long gameId, GameParticipant escapedThief) {
+        SystemEventData.EscapeData data = SystemEventData.EscapeData.of(escapedThief);
+        return SystemEvent.of(gameId, SystemEventType.ESCAPE, data);
     }
 }
