f03ec59445d3fd4b2de3470f5d2b2b2b4cf71369
HyerimH <gofla452@naver.com>
2026-02-09 16:11:11 +0900

    feat: SystemController 체포 API 추가 [#24]

--- src/main/java/com/team/cops_and_robbers/play/lobby/presentation/LobbyController.java
+++ src/main/java/com/team/cops_and_robbers/play/lobby/presentation/LobbyController.java
@@ -10,7 +10,6 @@ import com.team.cops_and_robbers.play.lobby.presentation.dto.ReadyUpdateRequest;
 import com.team.cops_and_robbers.play.lobby.presentation.dto.TeamChangeRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/games/{gameId}")
 @RequiredArgsConstructor
---
+++ src/main/java/com/team/cops_and_robbers/play/system/presentation/SystemController.java
@@ -0,0 +1,33 @@
+package com.team.cops_and_robbers.play.system.presentation;
+
+import com.team.cops_and_robbers.auth.presentation.annotation.AuthUser;
+import com.team.cops_and_robbers.auth.presentation.resolver.LoginUser;
+import com.team.cops_and_robbers.play.system.application.SystemService;
+import com.team.cops_and_robbers.play.system.application.dto.ArrestCommand;
+import com.team.cops_and_robbers.play.system.presentation.dto.ArrestRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/games/{gameId}/system")
+@RequiredArgsConstructor
+public class SystemController {
+
+    private final SystemService systemService;
+
+    /**
+     * 1. 경찰이 도둑을 체포합니다.
+     */
+    @PostMapping("/arrest")
+    public ResponseEntity<Void> arrestRobber(
+            @AuthUser LoginUser loginUser,
+            @PathVariable Long gameId,
+            @RequestBody @Valid ArrestRequest request
+    ) {
+        ArrestCommand command = ArrestCommand.of(gameId, loginUser.userId(), request.robberParticipantId());
+        systemService.arrestRobber(command);
+        return ResponseEntity.ok().build();
+    }
+}
