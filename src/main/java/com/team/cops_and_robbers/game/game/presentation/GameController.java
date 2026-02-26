c2393e37bc82c760efbdfab4349a5e5ec1953c73
HyerimH <gofla452@naver.com>
2026-02-26 23:57:58 +0900

    feat: 게임 정보 조회 API 컨트롤러 메서드 작성 [#40]

--- src/main/java/com/team/cops_and_robbers/game/game/presentation/GameController.java
+++ src/main/java/com/team/cops_and_robbers/game/game/presentation/GameController.java
@@ -4,13 +4,18 @@ import com.team.cops_and_robbers.auth.presentation.annotation.AuthUser;
 import com.team.cops_and_robbers.auth.presentation.resolver.LoginUser;
 import com.team.cops_and_robbers.game.game.application.GameService;
 import com.team.cops_and_robbers.game.game.application.dto.command.GameCreateCommand;
+import com.team.cops_and_robbers.game.game.application.dto.command.GameInfoCommand;
 import com.team.cops_and_robbers.game.game.application.dto.result.GameCreateResult;
+import com.team.cops_and_robbers.game.game.application.dto.result.GameInfoResult;
 import com.team.cops_and_robbers.game.game.presentation.dto.request.GameCreateRequest;
 import com.team.cops_and_robbers.game.game.presentation.dto.response.GameCreateResponse;
+import com.team.cops_and_robbers.game.game.presentation.dto.response.GameInfoResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +40,20 @@ public class GameController implements GameControllerDocs {
         GameCreateResult result = gameService.createGame(command);
         GameCreateResponse response = GameCreateResponse.from(result);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * 2. 게임 기본 설정 정보를 조회합니다.
+     */
+    @GetMapping("/{gameId}")
+    public ResponseEntity<GameInfoResponse> getGameInfo(
+            @AuthUser LoginUser loginUser,
+            @PathVariable Long gameId
+    ) {
+        GameInfoCommand command = GameInfoCommand.of(loginUser.userId(), gameId);
+        GameInfoResult result = gameService.getGameInfo(command);
+        GameInfoResponse response = GameInfoResponse.from(result);
+        return ResponseEntity.ok(response);
     }
 
 }
