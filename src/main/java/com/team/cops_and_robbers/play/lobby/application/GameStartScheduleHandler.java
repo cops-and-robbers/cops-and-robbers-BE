7a17fde19e7fcfdb082758774c7272752e3ea8e9
chxghee <francis001021@gmail.com>
2026-04-12 13:29:42 +0900

    refactor: game start 이벤트 시 캐시 적재 -> 이벤트 알림 메세지 publish 순서 보장 로직 추가 [#74]

--- src/main/java/com/team/cops_and_robbers/play/lobby/application/GameStartScheduleHandler.java
+++ src/main/java/com/team/cops_and_robbers/play/lobby/application/GameStartScheduleHandler.java
@@ -5,6 +5,7 @@ import com.team.cops_and_robbers.play.lobby.domain.LobbyEvent;
 import com.team.cops_and_robbers.play.lobby.domain.LobbyEventType;
 import com.team.cops_and_robbers.play.system.application.GameSchedulerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -16,6 +17,7 @@ public class GameStartScheduleHandler {
     private final GameSchedulerService gameSchedulerService;
     private final InGameParticipantCacheService inGameParticipantCacheService;
 
+    @Order(1)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleGameStartSchedule(LobbyEvent event) {
         if (event.type() == LobbyEventType.GAME_START) {
