0739765bc80b7a1ae14589cdeffaefd9c0736725
chxghee <francis001021@gmail.com>
2026-04-15 22:34:02 +0900

    docs: 신 기능 추가 및 로그인 응답 필드 변경에 따른 swagger 버전 업 2.0.0 [#78]

--- src/main/java/com/team/cops_and_robbers/common/swagger/SwaggerConfig.java
+++ src/main/java/com/team/cops_and_robbers/common/swagger/SwaggerConfig.java
@@ -30,10 +30,11 @@ public class SwaggerConfig {
 
         Info info = new Info()
                 .title("👮 경찰과 도둑 API 🥷")
-                .version("1.2.0")
+                .version("2.0.0")
                 .description("""
                         ## 업데이트 항목
-                        - 도둑 위치 조회 API 추가 (GET /api/games/{gameId}/robbers/location)
+                        - 이용 약관 동의 API 추가 (POST /api/user/agreements)
+                        - 로그인 API 필수 이용약관 동의 여부 응답 필드 추가 (POST /api/auth/login)
                         """);
 
         return new OpenAPI()
