5c801e632310cf10aa5014396ff062a055c4dc1f
chxghee <francis001021@gmail.com>
2026-04-16 17:38:48 +0900

    refactor: 원시타입 -> Boolean 으로 수정 [#78]

--- src/main/java/com/team/cops_and_robbers/user/application/dto/command/AgreementCommand.java
+++ src/main/java/com/team/cops_and_robbers/user/application/dto/command/AgreementCommand.java
@@ -2,19 +2,19 @@ package com.team.cops_and_robbers.user.application.dto.command;
 
 public record AgreementCommand(
         Long userId,
-        boolean termsOfService,
-        boolean privacyPolicy,
-        boolean locationTerms,
-        boolean marketing
+        Boolean termsOfService,
+        Boolean privacyPolicy,
+        Boolean locationTerms,
+        Boolean marketing
 ) {
     public boolean hasRequiredTermsAgreed() {
         return termsOfService && privacyPolicy && locationTerms;
     }
 
     public static AgreementCommand of(
-            Long userId, boolean termsOfService,
-            boolean privacyPolicy, boolean locationTerms,
-            boolean marketing
+            Long userId, Boolean termsOfService,
+            Boolean privacyPolicy, Boolean locationTerms,
+            Boolean marketing
     ) {
         return new AgreementCommand(
                 userId,
--- src/main/java/com/team/cops_and_robbers/user/presentation/dto/request/AgreementRequest.java
+++ src/main/java/com/team/cops_and_robbers/user/presentation/dto/request/AgreementRequest.java
@@ -6,15 +6,15 @@ import jakarta.validation.constraints.NotNull;
 public record AgreementRequest(
         @Schema(description = "이용약관 동의 여부", example = "true")
         @NotNull(message = "이용약관 동의 여부는 필수 입력 항목입니다.")
-        boolean termsOfService,
+        Boolean termsOfService,
         @Schema(description = "개인정보처리방침 동의 여부", example = "true")
         @NotNull(message = "개인정보처리방침 동의 여부는 필수 입력 항목입니다.")
-        boolean privacyPolicy,
+        Boolean privacyPolicy,
         @Schema(description = "위치정보 이용약관 동의 여부", example = "true")
         @NotNull(message = "위치정보 이용약관 동의 여부는 필수 입력 항목입니다.")
-        boolean locationTerms,
+        Boolean locationTerms,
         @Schema(description = "마케팅 수신 동의 여부", example = "true")
         @NotNull(message = "마케팅 수신 동의 여부는 필수 입력 항목입니다.")
-        boolean marketing
+        Boolean marketing
 ) {
 }
