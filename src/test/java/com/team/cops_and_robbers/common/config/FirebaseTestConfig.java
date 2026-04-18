a3f1898e4cea85cb3ab4b269ba4678df0dc21a94
HyerimH <gofla452@naver.com>
2026-04-18 16:21:32 +0900

    chore: FirebaseMessaging Spring Bean 등록 [#80]

--- src/test/java/com/team/cops_and_robbers/common/config/FirebaseTestConfig.java
+++ src/test/java/com/team/cops_and_robbers/common/config/FirebaseTestConfig.java
@@ -1,6 +1,7 @@
 package com.team.cops_and_robbers.common.config;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -16,5 +17,11 @@ public class FirebaseTestConfig {
     @Primary
     public FirebaseAuth firebaseAuth() {
         return mock(FirebaseAuth.class);
+    }
+
+    @Bean
+    @Primary
+    public FirebaseMessaging firebaseMessaging() {
+        return mock(FirebaseMessaging.class);
     }
 }
