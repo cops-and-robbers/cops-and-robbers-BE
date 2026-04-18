9df0a827b6f0b1516748eb04d56e380ed6040843
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2026-04-18 14:37:58 +0900

    docs: 공지사항 스웨거 버전 업데이트 [#81]

--- src/main/java/com/team/cops_and_robbers/common/swagger/SwaggerConfig.java
+++ src/main/java/com/team/cops_and_robbers/common/swagger/SwaggerConfig.java
@@ -30,10 +30,10 @@ public class SwaggerConfig {
 
         Info info = new Info()
                 .title("👮 경찰과 도둑 API 🥷")
-                .version("1.2.0")
+                .version("1.3.0")
                 .description("""
                         ## 업데이트 항목
-                        - 도둑 위치 조회 API 추가 (GET /api/games/{gameId}/robbers/location)
+                        - 공지사항 CRUD API 추가 (POST/GET/PATCH/DELETE /api/notices)
                         """);
 
         return new OpenAPI()
