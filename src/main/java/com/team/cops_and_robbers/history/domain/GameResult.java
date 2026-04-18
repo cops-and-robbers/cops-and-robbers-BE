df381448a23ed1e26680ca81da1b8bd723282671
HyerimH <gofla452@naver.com>
2026-04-19 02:22:58 +0900

    feat: GameResult 필드 변경 [#85]

--- src/main/java/com/team/cops_and_robbers/history/domain/GameResult.java
+++ src/main/java/com/team/cops_and_robbers/history/domain/GameResult.java
@@ -9,8 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Builder;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 import org.locationtech.jts.geom.Point;
 
 import java.time.Duration;
@@ -51,11 +49,6 @@ public class GameResult extends BaseTimeEntity {
     @Column(nullable = false)
     private Integer durationSeconds;
 
-    // TODO: 어떤 정보를 저장하고 통계를 낼지 추후 구체화
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(columnDefinition = "jsonb")
-    private GameHighlights highlights;
-
     @Column(nullable = false, columnDefinition = "GEOMETRY(POINT, 4326)")
     private Point playgroundCenter;
 
@@ -69,13 +62,11 @@ public class GameResult extends BaseTimeEntity {
             Integer totalPolice,
             Integer totalRobber,
             Integer capturedRobber,
-            GameHighlights highlights,
             Point center,
             Integer radius
     ) {
-        LocalDateTime now = LocalDateTime.now();        // 임시~~
         int durationSeconds = (int) Duration
-                .between(game.getStartedAt(), now)
+                .between(game.getStartedAt(), LocalDateTime.now())
                 .getSeconds();
 
         return GameResult.builder()
@@ -86,7 +77,6 @@ public class GameResult extends BaseTimeEntity {
                 .totalRobberCount(totalRobber)
                 .arrestedRobberCount(capturedRobber)
                 .durationSeconds(durationSeconds)
-                .highlights(highlights)
                 .playgroundCenter(center)
                 .playgroundRadiusInMeters(radius)
                 .build();
