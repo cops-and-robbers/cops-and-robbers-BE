53bbbdfbd048dcb41c3b121da214ae4e7451efa0
HyerimH <gofla452@naver.com>
2026-04-19 02:22:08 +0900

    feat: 게임 결과 조회 API 컨트롤러 메서드 작성 [#85]

---
+++ src/main/java/com/team/cops_and_robbers/history/presentation/GameResultController.java
@@ -0,0 +1,31 @@
+package com.team.cops_and_robbers.history.presentation;
+
+import com.team.cops_and_robbers.auth.presentation.annotation.AuthUser;
+import com.team.cops_and_robbers.auth.presentation.resolver.LoginUser;
+import com.team.cops_and_robbers.history.application.GameResultService;
+import com.team.cops_and_robbers.history.application.dto.result.GameResultResult;
+import com.team.cops_and_robbers.history.presentation.dto.response.GameResultResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/game-results")
+@RequiredArgsConstructor
+public class GameResultController implements GameResultControllerDocs {
+
+    private final GameResultService gameResultService;
+
+    @GetMapping("/{gameResultId}")
+    public ResponseEntity<GameResultResponse> getGameResult(
+            @AuthUser LoginUser loginUser,
+            @PathVariable Long gameResultId
+    ) {
+        GameResultResult result = gameResultService.getGameResult(gameResultId, loginUser.userId());
+        GameResultResponse response = GameResultResponse.from(result);
+        return ResponseEntity.ok(response);
+    }
+}
