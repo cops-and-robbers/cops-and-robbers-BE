32957a02c621f7e3ec2bc704cf6a9963da1fd139
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2026-04-30 13:42:51 +0900

    feat: 공지사항 권한 검증 403 에러 추가 [#81]

--- src/main/java/com/team/cops_and_robbers/notice/exception/NoticeException.java
+++ src/main/java/com/team/cops_and_robbers/notice/exception/NoticeException.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum NoticeException implements ExceptionCode {
 
-    NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "공지사항을 찾을 수 없음", "해당 공지사항을 찾을 수 없습니다.");
+    NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "공지사항을 찾을 수 없음", "해당 공지사항을 찾을 수 없습니다."),
+    FORBIDDEN_ADMIN_ONLY(HttpStatus.FORBIDDEN, "권한 없음", "관리자 권한이 필요합니다.");
 
     private final HttpStatus httpStatus;
     private final String title;
