fe869119d5a5e7455f8d07f89244ea42e24d54f5
HyerimH <gofla452@naver.com>
2026-06-28 14:06:49 +0900

    refactor: 이벤트 게임 신규 경찰 상태를 항상 ALIVE로 고정 [#109]

--- src/main/java/com/team/cops_and_robbers/game/participant/domain/GameParticipant.java
+++ src/main/java/com/team/cops_and_robbers/game/participant/domain/GameParticipant.java
@@ -3,24 +3,8 @@ package com.team.cops_and_robbers.game.participant.domain;
 import com.team.cops_and_robbers.common.BaseTimeEntity;
 import com.team.cops_and_robbers.game.game.domain.Game;
 import com.team.cops_and_robbers.user.domain.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "participants")
@@ -67,13 +51,11 @@ public class GameParticipant extends BaseTimeEntity {
     }
 
     public static GameParticipant createEventModeParticipant(Game game, User user) {
-        boolean policeMoveStarted = LocalDateTime.now().isAfter(game.getPoliceMoveStartTime());
-        ParticipantStatus status = policeMoveStarted ? ParticipantStatus.ALIVE : ParticipantStatus.POLICE_WAITING;
         return GameParticipant.builder()
                 .game(game)
                 .user(user)
                 .team(Team.POLICE)
-                .status(status)
+                .status(ParticipantStatus.ALIVE)
                 .isReady(true)
                 .isHost(false)
                 .build();
