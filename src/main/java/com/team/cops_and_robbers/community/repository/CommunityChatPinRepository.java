773d1a6e66e03d20d64b1c7648b994f2decb0608
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2026-08-30 14:38:08 +0900

    feat: 고정 채팅 엔티티와 테이블 추가 [#186]

---
+++ src/main/java/com/team/cops_and_robbers/community/repository/CommunityChatPinRepository.java
@@ -0,0 +1,18 @@
+package com.team.cops_and_robbers.community.repository;
+
+import com.team.cops_and_robbers.community.domain.CommunityChatPin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface CommunityChatPinRepository extends JpaRepository<CommunityChatPin, Long> {
+
+    Optional<CommunityChatPin> findByCommunityPostId(Long communityPostId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from CommunityChatPin p where p.communityPostId = :postId")
+    void deleteByCommunityPostId(@Param("postId") Long postId);
+}
