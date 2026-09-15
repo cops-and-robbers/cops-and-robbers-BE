f907f84c7c98759db8e9344ff76891ce17923b72
HyerimH <gofla452@naver.com>
2026-09-16 02:10:28 +0900

    test: 체포수 픽스처도 기본값을 명시하도록 보완 [#205]

--- src/test/java/com/team/cops_and_robbers/common/fixture/GameResultParticipantFixture.java
+++ src/test/java/com/team/cops_and_robbers/common/fixture/GameResultParticipantFixture.java
@@ -14,6 +14,7 @@ public class GameResultParticipantFixture {
                 .nickname(nickname)
                 .team(Team.POLICE)
                 .status(ParticipantStatus.ALIVE)
+                .arrestCount(0)
                 .build();
     }
 
@@ -24,6 +25,7 @@ public class GameResultParticipantFixture {
                 .nickname(nickname)
                 .team(Team.ROBBER)
                 .status(ParticipantStatus.JAILED)
+                .arrestCount(0)
                 .build();
     }
 }
